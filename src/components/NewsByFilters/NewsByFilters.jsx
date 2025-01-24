191ad4eca97031945cb8e64187a631555310e233
Potompridumau <vfvfhsitv@gmail.com>
2025-01-24 13:33:06 +0300

    fix slider

--- src/components/NewsByFilters/NewsByFilters.jsx
+++ src/components/NewsByFilters/NewsByFilters.jsx
@@ -1,12 +1,31 @@
-import { TOTAL_PAGES } from '../../constant/constant';
+import { getNews } from '../../api/apiNews';
+import { PAGE_SIZE, TOTAL_PAGES } from '../../constant/constant';
+import { useDebounce } from '../../helpers/hooks/useDebounce';
+import { useFetch } from '../../helpers/hooks/useFetch';
+import { useFilters } from '../../helpers/hooks/useFilters';
 import NewsFilters from '../NewsFilters/NewsFilters';
 import NewsListWithSkeleton from '../NewsList/NewsList';
-import Pagination from '../Pagination/Pagination';
+import PaginationWrapper from '../PaginationWrapper/PaginationWrapper';
 
 import styles from './styles.module.css'
 
-const NewsByFilters = ({ filters, changeFilter, isLoading, news }) => {
+const NewsByFilters = () => {
 
+    const { filters, changeFilter } = useFilters({
+        page_number: 1,
+        page_size: PAGE_SIZE,
+        category: null,
+        keywords: ''
+    })
+
+    const debouncedKeywords = useDebounce(filters.keywords, 1500)
+
+    const { data, isLoading } = useFetch(getNews,
+        {
+            ...filters,
+            keywords: debouncedKeywords
+        }
+    )
 
     const handleNextPage = () => {
         if (filters.page_number < TOTAL_PAGES) {
@@ -28,23 +47,18 @@ const NewsByFilters = ({ filters, changeFilter, isLoading, news }) => {
         <section className={styles.section}>
             <NewsFilters filters={filters} changeFilter={changeFilter} />
 
-            <Pagination
+            <PaginationWrapper
+                top
+                bottom
                 handleNextPage={handleNextPage}
                 handlePrevPage={handlePrevPage}
                 handlePageClick={handlePageClick}
                 totalPages={TOTAL_PAGES}
                 currentPage={filters.page_number}
-            />
+            >
+                <NewsListWithSkeleton isLoading={isLoading} news={data?.news} />
+            </PaginationWrapper>
 
-            <NewsListWithSkeleton isLoading={isLoading} news={news} />
-
-            <Pagination
-                handleNextPage={handleNextPage}
-                handlePrevPage={handlePrevPage}
-                handlePageClick={handlePageClick}
-                totalPages={TOTAL_PAGES}
-                currentPage={filters.page_number}
-            />
         </section>
     );
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,42 +1,14 @@
-import { getNews } from '../../api/apiNews'
-import { useDebounce } from '../../helpers/hooks/useDebounce';
-import { useFetch } from '../../helpers/hooks/useFetch';
-import { useFilters } from '../../helpers/hooks/useFilters';
-import { PAGE_SIZE } from '../../constant/constant';
-
 import LatestNews from '../../components/LatestNews/LatestNews';
 import NewsByFilters from '../../components/NewsByFilters/NewsByFilters';
 
 import styles from './styles.module.css';
 
 const Main = () => {
-
-    const { filters, changeFilter } = useFilters({
-        page_number: 1,
-        page_size: PAGE_SIZE,
-        category: null,
-        keywords: ''
-    })
-
-    const debouncedKeywords = useDebounce(filters.keywords, 1500)
-
-    const { data, isLoading } = useFetch(getNews,
-        {
-            ...filters,
-            keywords: debouncedKeywords
-        }
-    )
-
     return (
         <main className={styles.main}>
-            <LatestNews isLoading={isLoading} banners={data && data.news} />
+            <LatestNews />
 
-            <NewsByFilters
-                news={data?.news}
-                isLoading={isLoading}
-                filters={filters}
-                changeFilter={changeFilter}
-            />
+            <NewsByFilters />
         </main>
     );
 }
